1e5fd00c8856d3919040947987f718571b4fd512
SUNSK <senkk512@163.com>
2020-07-23 11:53:22 +0800

    [FizzBizz][Sky] feature: implement should_return_fizzbizz_when_play_fizz_bizz_given_15 return fizzbizz

--- src/main/java/junit5/tdd/FizzBizz.java
+++ src/main/java/junit5/tdd/FizzBizz.java
@@ -1,17 +1,19 @@
 package junit5.tdd;
 
 
-
 public class FizzBizz {
     public String playFizzBizz(int number) {
         //String result = "";
-        if(number %3 == 0){
-            return  "fizz";
+        if (number % 3 == 0 && number % 5 == 0){
+            return "fizzbizz";
         }
-        if(number %5 == 0){
-            return  "bizz";
+        if (number % 3 == 0) {
+            return "fizz";
+        }
+        if (number % 5 == 0) {
+            return "bizz";
         }
 
-        return String.valueOf(number);
+            return String.valueOf(number);
     }
 }
